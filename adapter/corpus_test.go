7dd8c5989de1ecf2cb05e5ef3af4ad4de67bf877
Snowlights <wei1109942647@qq.com>
2020-03-24 20:31:13 +0800

    update

--- adapter/corpus_test.go
+++ adapter/corpus_test.go
@@ -6,14 +6,14 @@ import (
 	corpus "github.com/Snowlights/pub/grpc"
 	"testing"
 )
-
+//ONkSy6HRfzRX7w1n
 func TestLoginUser(t *testing.T) {
 	ctx := context.Background()
 	req := &corpus.LoginUserReq{
-		EMail:                "858777157@qq.com",
-		UserPassword:         "woaini12",
-		Phone:                "",
-		Code:                 0,
+		EMail:                "",
+		UserPassword:         "",
+		Phone:                "18846082154",
+		Code:                 831950,
 	}
 
 	res := LoginUser(ctx,req)
@@ -21,4 +21,63 @@ func TestLoginUser(t *testing.T) {
 		fmt.Printf("%v",res.Errinfo.Msg)
 	}
 	fmt.Println(res)
+}
+
+func TestSendMessage(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.SendMessageReq{
+		Phone:                "18846082154",
+	}
+	res := SendMessage(ctx,req)
+	if res.Errinfo != nil{
+		fmt.Printf("%v",res.Errinfo.Msg)
+	}
+	fmt.Println(res)
+}
+
+func TestListUserInfo(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.ListUserInfoReq{
+		Offset:               0,
+		Limit:                10,
+		EMail:                "",
+		UserName:             "",
+		Phone:                "",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := ListUserInfo(ctx,req)
+	if res.Errinfo != nil{
+		fmt.Printf("%v",res.Errinfo.Msg)
+	}
+	fmt.Println(res)
+}
+func TestUpdateUserInfo(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.UpdateUserInfoReq{
+		UserId:               4,
+		UserName:             "花开雪夜",
+		Phone:                "18846082154",
+		EMail:                "858777157@qq.com",
+		Description:          "我爱她她吗",
+		Cookie:               "ONkSy6HRfzRX7w1n",
+	}
+	res := UpdateUserInfo(ctx,req)
+	if res.Errinfo != nil{
+		fmt.Printf("%v",res.Errinfo.Msg)
+	}
+	fmt.Printf("%v",res)
+
+}
+
+func TestDelUserInfo(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.DelUserInfoReq{
+		UserId:               0,
+		Cookie:               "",
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	res := DelUserInfo(ctx,req)
+	fmt.Printf("%v",res)
 }
