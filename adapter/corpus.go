3cc26fe27157cba56f1582b2d605cf1eb93fc73e
Snowlights <wei1109942647@qq.com>
2020-03-26 20:55:10 +0800

    fixuser

--- adapter/corpus.go
+++ adapter/corpus.go
@@ -13,6 +13,12 @@ const (
 	address     = "localhost:50051"
 	defaultName = "corpus"
 )
+var client corpus.CorpusServiceClient
+
+func initLogic(){
+	ctx := context.Background()
+	Prepare(ctx)
+}
 
 func initConnect(ctx context.Context) *grpc.ClientConn{
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
@@ -20,6 +26,11 @@ func initConnect(ctx context.Context) *grpc.ClientConn{
 		log.Fatalf("did not connect: %v", err)
 	}
 	return conn
+}
+
+func Prepare(ctx context.Context){
+	conn := initConnect(ctx)
+	client = corpus.NewCorpusServiceClient(conn)
 }
 
 func getHashKey(ctx context.Context) string {
@@ -39,9 +50,7 @@ func getHashKey(ctx context.Context) string {
 func LoginUser(ctx context.Context,req *corpus.LoginUserReq) (res *corpus.LoginUserRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.LoginUser(ctx,req)
+	res, err := client.LoginUser(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil {
 		res = &corpus.LoginUserRes{
@@ -65,9 +74,7 @@ func LoginUser(ctx context.Context,req *corpus.LoginUserReq) (res *corpus.LoginU
 func LogoutUserInfo(ctx context.Context,req *corpus.LogoutUserInfoReq) (res *corpus.LogoutUserInfoRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.LogoutUserInfo(ctx,req)
+	res, err := client.LogoutUserInfo(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.LogoutUserInfoRes{
@@ -87,12 +94,34 @@ func LogoutUserInfo(ctx context.Context,req *corpus.LogoutUserInfoReq) (res *cor
 	return
 }
 
+func UpdateUserPhone(ctx context.Context,req *corpus.UpdateUserPhoneReq) (res *corpus.UpdateUserPhoneRes){
+	routeKey := getHashKey(ctx)
+	log.Printf("%v routekey ",routeKey)
+	res, err := client.UpdateUserPhone(ctx,req)
+	log.Printf("%v success %v",ctx,res)
+	if err != nil{
+		res = &corpus.UpdateUserPhoneRes{
+			Errinfo:              &corpus.ErrorInfo{
+				Ret:                  -1,
+				Msg:                  err.Error(),
+				XXX_NoUnkeyedLiteral: struct{}{},
+				XXX_unrecognized:     nil,
+				XXX_sizecache:        0,
+			},
+			Data:                 nil,
+			XXX_NoUnkeyedLiteral: struct{}{},
+			XXX_unrecognized:     nil,
+			XXX_sizecache:        0,
+		}
+	}
+	return
+}
+
+
 func SendMessage(ctx context.Context,req *corpus.SendMessageReq) (res *corpus.SendMessageRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.SendMessage(ctx,req)
+	res, err := client.SendMessage(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.SendMessageRes{
@@ -115,9 +144,7 @@ func SendMessage(ctx context.Context,req *corpus.SendMessageReq) (res *corpus.Se
 func UpdateUserInfo(ctx context.Context,req *corpus.UpdateUserInfoReq)(res *corpus.UpdateUserInfoRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.UpdateUserInfo(ctx,req)
+	res, err := client.UpdateUserInfo(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.UpdateUserInfoRes{
@@ -140,9 +167,7 @@ func UpdateUserInfo(ctx context.Context,req *corpus.UpdateUserInfoReq)(res *corp
 func DelUserInfo(ctx context.Context,req *corpus.DelUserInfoReq)(res *corpus.DelUserInfoRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.DelUserInfo(ctx,req)
+	res, err := client.DelUserInfo(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.DelUserInfoRes{
@@ -165,9 +190,8 @@ func DelUserInfo(ctx context.Context,req *corpus.DelUserInfoReq)(res *corpus.Del
 func ListUserInfo(ctx context.Context,req *corpus.ListUserInfoReq) (res *corpus.ListUserInfoRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.ListUserInfo(ctx,req)
+
+	res, err := client.ListUserInfo(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.ListUserInfoRes{
@@ -191,9 +215,7 @@ func ListUserInfo(ctx context.Context,req *corpus.ListUserInfoReq) (res *corpus.
 func AddAdminUser(ctx context.Context,req *corpus.AddAdminUserReq)(res *corpus.AddAdminUserRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.AddAdminUser(ctx,req)
+	res, err := client.AddAdminUser(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.AddAdminUserRes{
@@ -216,9 +238,7 @@ func AddAdminUser(ctx context.Context,req *corpus.AddAdminUserReq)(res *corpus.A
 func DelAdminUser(ctx context.Context,req *corpus.DelAdminUserReq)(res *corpus.DelAdminUserRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.DelAdminUser(ctx,req)
+	res, err := client.DelAdminUser(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.DelAdminUserRes{
@@ -242,9 +262,7 @@ func DelAdminUser(ctx context.Context,req *corpus.DelAdminUserReq)(res *corpus.D
 func ListAdminUser(ctx context.Context,req *corpus.ListAdminUserReq)(res *corpus.ListAdminUserRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.ListAdminUser(ctx,req)
+	res, err := client.ListAdminUser(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.ListAdminUserRes{
@@ -267,9 +285,7 @@ func ListAdminUser(ctx context.Context,req *corpus.ListAdminUserReq)(res *corpus
 func AddAuth(ctx context.Context,req *corpus.AddAuthReq)(res *corpus.AddAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.AddAuth(ctx,req)
+	res, err := client.AddAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.AddAuthRes{
@@ -292,9 +308,7 @@ func AddAuth(ctx context.Context,req *corpus.AddAuthReq)(res *corpus.AddAuthRes)
 func UpdateAuth(ctx context.Context,req *corpus.UpdateAuthReq)(res *corpus.UpdateAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.UpdateAuth(ctx,req)
+	res, err := client.UpdateAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.UpdateAuthRes{
@@ -317,9 +331,7 @@ func UpdateAuth(ctx context.Context,req *corpus.UpdateAuthReq)(res *corpus.Updat
 func DelAuth(ctx context.Context,req *corpus.DelAuthReq) (res *corpus.DelAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.DelAuth(ctx,req)
+	res, err := client.DelAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err!= nil{
 		res = &corpus.DelAuthRes{
@@ -342,9 +354,7 @@ func DelAuth(ctx context.Context,req *corpus.DelAuthReq) (res *corpus.DelAuthRes
 func ListAuth(ctx context.Context,req *corpus.ListAuthReq)(res *corpus.ListAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.ListAuth(ctx,req)
+	res, err := client.ListAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.ListAuthRes{
@@ -367,9 +377,7 @@ func ListAuth(ctx context.Context,req *corpus.ListAuthReq)(res *corpus.ListAuthR
 func AddUserAuth(ctx context.Context,req *corpus.AddUserAuthReq) (res *corpus.AddUserAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.AddUserAuth(ctx,req)
+	res, err := client.AddUserAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.AddUserAuthRes{
@@ -392,9 +400,7 @@ func AddUserAuth(ctx context.Context,req *corpus.AddUserAuthReq) (res *corpus.Ad
 func DelUserAuth(ctx context.Context,req *corpus.DelUserAuthReq)(res *corpus.DelUserAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.DelUserAuth(ctx,req)
+	res, err := client.DelUserAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.DelUserAuthRes{
@@ -417,9 +423,7 @@ func DelUserAuth(ctx context.Context,req *corpus.DelUserAuthReq)(res *corpus.Del
 func ListUserAuth(ctx context.Context,req *corpus.ListUserAuthReq)(res *corpus.ListUserAuthRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.ListUserAuth(ctx,req)
+	res, err := client.ListUserAuth(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.ListUserAuthRes{
@@ -442,9 +446,7 @@ func ListUserAuth(ctx context.Context,req *corpus.ListUserAuthReq)(res *corpus.L
 func AddTransAudio(ctx context.Context,req *corpus.AddTransAudioReq)(res *corpus.AddTransAudioRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.AddTransAudio(ctx,req)
+	res, err := client.AddTransAudio(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.AddTransAudioRes{
@@ -467,9 +469,7 @@ func AddTransAudio(ctx context.Context,req *corpus.AddTransAudioReq)(res *corpus
 func DelTransAudio(ctx context.Context,req *corpus.DelTransAudioReq)(res *corpus.DelTransAudioRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.DelTransAudio(ctx,req)
+	res, err := client.DelTransAudio(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.DelTransAudioRes{
@@ -492,9 +492,7 @@ func DelTransAudio(ctx context.Context,req *corpus.DelTransAudioReq)(res *corpus
 func ListTransAudio(ctx context.Context,req *corpus.ListTransAudioReq)(res* corpus.ListTransAudioRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.ListTransAudio(ctx,req)
+	res, err := client.ListTransAudio(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.ListTransAudioRes{
@@ -517,9 +515,7 @@ func ListTransAudio(ctx context.Context,req *corpus.ListTransAudioReq)(res* corp
 func GetKeyWord(ctx context.Context,req *corpus.GetKeyWordReq)(res *corpus.GetKeyWordRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.GetKeyWord(ctx,req)
+	res, err := client.GetKeyWord(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.GetKeyWordRes{
@@ -567,9 +563,7 @@ func Evaluation(ctx context.Context,req *corpus.EvaluationReq)(res *corpus.Evalu
 func RecognizeImage(ctx context.Context,req *corpus.RecognizeImageReq)(res *corpus.RecognizeImageRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.RecognizeImage(ctx,req)
+	res, err := client.RecognizeImage(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.RecognizeImageRes{
@@ -592,9 +586,7 @@ func RecognizeImage(ctx context.Context,req *corpus.RecognizeImageReq)(res *corp
 func TreansAudioToText(ctx context.Context,req *corpus.TransAudioToTextReq)(res *corpus.TransAudioToTextRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.TransAudioToText(ctx,req)
+	res, err := client.TransAudioToText(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.TransAudioToTextRes{
@@ -617,9 +609,7 @@ func TreansAudioToText(ctx context.Context,req *corpus.TransAudioToTextReq)(res 
 func RecognizeAge(ctx context.Context,req *corpus.RecognizeAgeReq)(res *corpus.RecognizeAgeRes){
 	routeKey := getHashKey(ctx)
 	log.Printf("%v routekey ",routeKey)
-	conn := initConnect(ctx)
-	c := corpus.NewCorpusServiceClient(conn)
-	res, err := c.RecognizeAge(ctx,req)
+	res, err := client.RecognizeAge(ctx,req)
 	log.Printf("%v success %v",ctx,res)
 	if err != nil{
 		res = &corpus.RecognizeAgeRes{
--- adapter/corpus_test.go
+++ adapter/corpus_test.go
@@ -8,12 +8,13 @@ import (
 )
 //ONkSy6HRfzRX7w1n
 func TestLoginUser(t *testing.T) {
+	initLogic()
 	ctx := context.Background()
 	req := &corpus.LoginUserReq{
 		EMail:                "",
 		UserPassword:         "",
-		Phone:                "18846082154",
-		Code:                 831950,
+		Phone:                "15535259636",
+		Code:                 "343222",
 	}
 
 	res := LoginUser(ctx,req)
@@ -24,15 +25,27 @@ func TestLoginUser(t *testing.T) {
 }
 
 func TestSendMessage(t *testing.T) {
+	initLogic()
 	ctx := context.Background()
 	req := &corpus.SendMessageReq{
-		Phone:                "18846082154",
+		Phone:                "15535259636",
 	}
 	res := SendMessage(ctx,req)
 	if res.Errinfo != nil{
 		fmt.Printf("%v",res.Errinfo.Msg)
 	}
 	fmt.Println(res)
+}
+
+func TestUpdateUserInfo2(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.UpdateUserPhoneReq{
+		Phone:                "1555555545",
+		Code:                 "949116",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := UpdateUserPhone(ctx,req)
+	fmt.Printf("%v",res)
 }
 
 func TestListUserInfo(t *testing.T) {
@@ -42,7 +55,7 @@ func TestListUserInfo(t *testing.T) {
 		Limit:                10,
 		EMail:                "",
 		UserName:             "",
-		Phone:                "",
+		Phone:                "18846082154",
 		Cookie:               "JnL3gxsI402j4hs4",
 	}
 	res := ListUserInfo(ctx,req)
@@ -51,15 +64,29 @@ func TestListUserInfo(t *testing.T) {
 	}
 	fmt.Println(res)
 }
+
+func TestListUserInfo2(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.ListUserInfoReq{
+		Offset:               0,
+		Limit:                1,
+		EMail:                "",
+		UserName:             "",
+		Phone:                "18846082154",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := ListUserInfo(ctx,req)
+	fmt.Println(res)
+}
 func TestUpdateUserInfo(t *testing.T) {
 	ctx := context.Background()
 	req := &corpus.UpdateUserInfoReq{
-		UserId:               4,
-		UserName:             "花开雪夜",
-		Phone:                "18846082154",
+		UserId:               6,
+		UserName:             "美丽",
 		EMail:                "858777157@qq.com",
-		Description:          "我爱她她吗",
-		Cookie:               "ONkSy6HRfzRX7w1n",
+		Password:             "123456",
+		Description:          "好美丽雅",
+		Cookie:               "tTawF21P99KlqUAE",
 	}
 	res := UpdateUserInfo(ctx,req)
 	if res.Errinfo != nil{
@@ -69,11 +96,20 @@ func TestUpdateUserInfo(t *testing.T) {
 
 }
 
+func TestLogoutUserInfo(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.LogoutUserInfoReq{
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := LogoutUserInfo(ctx,req)
+	fmt.Printf("%v",res)
+}
+
 func TestDelUserInfo(t *testing.T) {
 	ctx := context.Background()
 	req := &corpus.DelUserInfoReq{
-		UserId:               0,
-		Cookie:               "",
+		UserId:               6,
+		Cookie:               "tTawF21P99KlqUAE",
 		XXX_NoUnkeyedLiteral: struct{}{},
 		XXX_unrecognized:     nil,
 		XXX_sizecache:        0,
@@ -81,3 +117,158 @@ func TestDelUserInfo(t *testing.T) {
 	res := DelUserInfo(ctx,req)
 	fmt.Printf("%v",res)
 }
+
+func TestAddAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.AddAuthReq{
+		AuthCode:             "SERVICE_AGE_CODE",
+		AuthDescription:      "年龄识别服务（主要用于内部，确定发展方向）",
+		ServiceName:          "/service/age",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := AddAuth(ctx,req)
+	fmt.Printf("%v\n",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestUpdateAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.UpdateAuthReq{
+		Id:                   6,
+		AuthCode:             "SERVICE_AUDIO_RECOGNIZE_CODE",
+		AuthDescription:      "音频识别服务",
+		ServiceName:          "/audio",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := UpdateAuth(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestDelAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.DelAuthReq{
+		Id:                   0,
+		AuthCode:             "SERVICE_AUDIO_RECOGNIZE_CODE",
+		Cookie:               "JnL3gxsI402j4hs4",
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	res := DelAuth(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestListAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.ListAuthReq{
+		Offset:               0,
+		Limit:                10,
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	res := ListAuth(ctx,req)
+	fmt.Printf("%v",res)
+}
+
+func TestAddAdminUser(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.AddAdminUserReq{
+		UserId:               8,
+		Cookie:               "JnL3gxsI402j4hs4",
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	res := AddAdminUser(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestDelAdminUser(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.DelAdminUserReq{
+		UserId:               8,
+		Cookie:               "JnL3gxsI402j4hs4",
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	res := DelAdminUser(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestListAdminUser(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.ListAdminUserReq{
+		Offset:               0,
+		Limit:                10,
+		Cookie:               "",
+	}
+	res := ListAdminUser(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+
+}
+
+func TestAddUserAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.AddUserAuthReq{
+		UserId:               4,
+		AuthCode:             "SERVICE_AUDIO_CODE",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := AddUserAuth(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestDelUserAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.DelUserAuthReq{
+		UserId:               4,
+		AuthCode:             "SERVICE_AUDIO_CODE",
+		Cookie:               "JnL3gxsI402j4hs4",
+	}
+	res := DelUserAuth(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
+func TestListUserAuth(t *testing.T) {
+	ctx := context.Background()
+	req := &corpus.ListUserAuthReq{
+		UserId:               4,
+		Limit:                10,
+		Offset:               0,
+		Cookie:               "",
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	res := ListUserAuth(ctx,req)
+	fmt.Printf("%v",res)
+	if res.Errinfo != nil{
+		fmt.Printf("%v\n",res.Errinfo.Msg)
+	}
+}
+
